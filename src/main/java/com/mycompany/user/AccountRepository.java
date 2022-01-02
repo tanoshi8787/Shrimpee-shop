c4cb0d988dba8edd1217a117d8bed3997fd7ec54
fcu-d0843407 <72053388+fcu-d0843407@users.noreply.github.com>
2022-01-02 20:29:00 +0800

    v1.05

--- src/main/java/com/mycompany/user/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface AccountRepository extends CrudRepository<Account, Integer> {
-    public Long countById(Integer id);
-}
