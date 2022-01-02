47a736c901a1ebcacb517f24a0107496ef9e8008
fcu-d0843407 <72053388+fcu-d0843407@users.noreply.github.com>
2022-01-02 16:23:30 +0800

    0.03 新增Account.java

---
+++ src/main/java/com/mycompany/user/Account.java
@@ -0,0 +1,60 @@
+package com.mycompany.user;
+
+import javax.persistence.*;
+
+@SuppressWarnings("ALL")
+@Entity
+@Table(name = "users_account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer AccountId;
+    @Column(length = 45, nullable = false, name = "AccountName")
+    private String AccountName;
+    private String AccountPhone;
+    private String AccountAddress;
+    private String Account;
+    private String AccountPassword;
+
+
+    public Integer getAccountId() {
+        return AccountId;
+    }
+    public void setAccountId(Integer account_id) {
+        this.AccountId = AccountId;
+    }
+
+    public String getAccountName() {
+        return AccountName;
+    }
+    public void setAccountName(String AccountName) {
+        this.AccountName = AccountName;
+    }
+
+
+    public String getAccountPhone(){return AccountPhone;}
+    public void setAccountPhone(String AccountPhone){this.AccountPhone = AccountPhone;}
+
+    public String getAccountAddress(){return AccountAddress;}
+    public void  setAccountAddress(String AccountAddress){this.AccountAddress = AccountAddress;}
+
+    public String getAccount(){return Account;}
+    public void setAccount(String Account){this.Account = Account;}
+
+    public String getAccountPassword(){return AccountPassword;}
+    public void setAccountPassword(String AccountPassword){this.AccountPassword = AccountPassword;}
+    //下面式幹嘛的
+//    @Override
+//    public String toAccountString() {
+//        return "User{" +
+//                "AccountId=" + getAccountId() +
+//
+//                ", AccountName='" + getAccountName() + '\'' +
+//                ", AccountPhone='" + getAccountPhone() +'\'' +
+//                ", AccountAddress='" + getAccountAddress() +'\'' +
+//                ", Account='" + getAccount() + '\'' +
+//                ", AccountPassword='" + getAccountPassword() + '\'' +
+//                '}';
+//    }
+
+}
